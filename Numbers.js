612c2869f914380d5464804397441c28540b4b10
Milind <33289554+Mi1ind@users.noreply.github.com>
2021-01-05 20:10:01 -0500

    js numerology calculator

---
+++ Numbers.js
@@ -0,0 +1,37 @@
+function Chaldean(word) {
+    var dict = {
+        "A": 1,
+        "B": 2,
+        "C": 3,
+        "D": 4,
+        "E": 5,
+        "F": 8,
+        "G": 3,
+        "H": 5,
+        "I": 1,
+        "J": 1,
+        "K": 2,
+        "L": 3,
+        "M": 4,
+        "N": 5,
+        "O": 7,
+        "P": 8,
+        "Q": 1,
+        "R": 2,
+        "S": 3,
+        "T": 4,
+        "U": 6,
+        "V": 6,
+        "W": 6,
+        "X": 5,
+        "Y": 1,
+        "Z": 7,
+        " ": 0
+    }
+    word = word.toUpperCase();
+    i = 0;
+    for (x = 0; i < word.length; i++) {
+        x += dict[word[i]]
+    }
+    return x;
+}
